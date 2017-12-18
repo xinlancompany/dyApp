92aea117c71313955e3ed2f33a05c29f00850757
Doriadeng <819414081@qq.com>
2017-12-18 10:10:18 +0800

    首页

--- js/index.js
+++ js/index.js
@@ -33,30 +33,10 @@ mui.init({
 var index = new Vue({
 	el: '#index',
 	data: {
-		scrollNews: [{
-			img: 'https://unsplash.it/750/200',
-			title: '一'
-		},{
-			img: 'https://unsplash.it/750/200',
-			title: '二'
-		}],
-		headNews: [{
-			img: 'https://unsplash.it/120/60',
-			title: '市委组织部一行赴普陀调研沈家门渔港特色小镇建设工作市委组织部一行赴普陀调研沈家门渔港特色小镇建设工作',
-			date: '2017-09-17'
-		},{
-			img: 'https://unsplash.it/120/60',
-			title: '市委组织部一行赴浦东国际人才城学习交流',
-			date: '2017-09-17'
-		}],
-		activity: [{
-			img: 'https://unsplash.it/750/250',
-			title: '市委组织部一行赴普陀调研沈家门渔港特色小镇建设工作市委组织部一行赴普陀调研沈家门渔港特色小镇建设工作',
-		},{
-			img: 'https://unsplash.it/750/250',
-			title: '市委组织部一行赴普陀调研沈家门渔港特色小镇建设工作市委组织部一行赴普陀调研沈家门渔港特色小镇建设工作',
-		}],
-		activeSlideText: '一'
+		scrollNews: [],
+		headNews: [],
+		activity: [], //活动专题
+		activeSlideText: ''
 	},
 	methods: {
 		gotoDetail: function(i) {
@@ -67,17 +47,57 @@ var index = new Vue({
 		},
 		goActivityTab: function() {
 			$('.go-activity').click();
+		},
+		//获取动态新闻
+		getNews: function(){
+			var self = this;
+			
+			_callAjax({
+				cmd:"fetch",
+				sql:"select id, title, img, content, linkerId, reporter, readcnt, newsdate, subtitle from articles where ifValid =1 and linkerId = " + linkerId.News +" order by id desc limit 10"
+			},function(d){
+				_tell(d.data);
+				if(d.success && d.data) {
+					if(d.data.length>5){
+						self.scrollNews = d.data.slice(0,5);
+						self.headNews = d.data.slice(5,8);
+					}else {
+						self.scrollNews = d.data;
+					}
+					
+					self.activeSlideText = self.scrollNews[0].title;
+					setTimeout(function(){
+						var swiper = new Swiper('.index-swiper', {
+							pagination: '.swiper-pagination',
+							onSlideChangeEnd: function(swiper){
+						      	self.activeSlideText = self.scrollNews[swiper.activeIndex].title
+							}
+						});
+					}, 500)
+				}
+			})
+		},
+		//获取活动专题
+		getActivitySort: function(){
+			var self = this;
+			
+			_callAjax({
+				cmd: "fetch",
+				sql: "select name, img, strftime('%Y-%m-%d %H:%M', logtime) as logtime from linkers where ifValid = 1 and refId = " + linkerId.activitySort + " order by id desc limit 2"
+			},function(d){
+				if(d.success && d.data){
+					self.activity = d.data
+				}
+			})
 		}
 	},
 	mounted: function() {
 		var self = this;
 		
-		var swiper = new Swiper('.index-swiper', {
-			pagination: '.index-pagination',
-			onSlideChangeEnd: function(swiper){
-		      	self.activeSlideText = self.scrollNews[swiper.activeIndex].title
-			}
-		});
+		//获取动态新闻
+		self.getNews();
+		//获取活动专题
+		self.getActivitySort();
 	}
 })
 
@@ -170,13 +190,40 @@ var ucenter = new Vue({
 	el: '#ucenter',
 	data: {
 		isLogin: false,
+		androidUpdate: false,
 		userInfo: {
 			
 		}
 	},
 	methods: {
+		//登录
 		goLogin: function() {
 			
-		}
+		},
+		//我的消息
+		goPost: function(){
+			
+		},
+		//我的学习
+		goMyStudy: function(){
+			
+		},
+		//我的活动
+		goMyActivity: function(){
+			
+		},
+		//推荐
+		goRecommend:function(){
+			
+		},
+		//退出登录
+		logout: function(){
+			
+		},
+		//清除缓存
+		clearCache:function(){
+			
+		},
+		
 	}
 })
