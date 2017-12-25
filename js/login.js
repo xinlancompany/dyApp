6e0dadcd3584f7f0b7b22a3195972b3c14816c20
Doriadeng <819414081@qq.com>
2017-12-25 09:54:37 +0800

    login

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ mui.init({
 
 // 扩展API加载完毕，现在可以正常调用扩展API
 function plusReady() {
+	
 }
 var login = new Vue({
 	el: '#login',
@@ -12,7 +13,20 @@ var login = new Vue({
 		password: '',  //密码
 	},
 	methods: {
-	
+		//登录
+		login:function(){
+			var self = this;
+			
+			_callAjax({
+				cmd:"fetch",
+				sql:"select id, name, img from User where name = ? and pswd = ?",
+				vals: _dump([self.username.trim(), self.password.trim()])
+			},function(d){
+				if(d.success && d.data){
+					mui.toast("登录成功");
+				}
+			})
+		}
 	},
 	mounted: function() {
 		var self = this;
